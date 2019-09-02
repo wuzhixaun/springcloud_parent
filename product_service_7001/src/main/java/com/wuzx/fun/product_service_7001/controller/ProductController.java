b5af5044dfa8fffde68c9c33ee6df9c046d0a464
wuzhixuan <1440745721@qq.com>
2019-09-03 00:31:23 +0800

    springcloud注册中心

--- product_service_7001/src/main/java/com/wuzx/fun/product_service_7001/controller/ProductController.java
+++ product_service_7001/src/main/java/com/wuzx/fun/product_service_7001/controller/ProductController.java
@@ -4,6 +4,8 @@ package com.wuzx.fun.product_service_7001.controller;
 import com.wuzx.fun.commonapi.domain.Product;
 import com.wuzx.fun.product_service_7001.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,8 +15,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = {"/api/product"})
+@RefreshScope//声明这例可以刷新
 public class ProductController {
 
+    @Value("${server.port}")
+    String port;
+
+    @Value("${env}")
+    String env;
     @Autowired
     private ProductService productService;
 
@@ -25,6 +33,7 @@ public class ProductController {
 
     @RequestMapping(value = "/get/{id}", method = RequestMethod.GET)
     public Product get(@PathVariable int id) {
+        System.out.println(port+env);
         return productService.get(id);
     }
 }
